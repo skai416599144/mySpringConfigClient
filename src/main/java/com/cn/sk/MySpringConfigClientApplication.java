139c8899193c7c1bbae8125d173e0fcfa9dea295
skai416599144 <416599144@qq.com>
2017-07-25 22:01:47 +0800

    code update

---
+++ src/main/java/com/cn/sk/MySpringConfigClientApplication.java
@@ -0,0 +1,22 @@
+package com.cn.sk;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MySpringConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringConfigClientApplication.class, args);
+	}
+	 @Value("${foo}")
+	    String foo;
+	    @RequestMapping(value = "/hi")
+	    public String hi(){
+	        return foo;
+	    }
+}
